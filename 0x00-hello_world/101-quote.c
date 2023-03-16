5908fa532fabdcf8493f58d370eaad8d3f852787
gideon607 <gideonojigi@gmail.com>
2023-03-16 17:32:10 +0100

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,14 @@
 #include <unistd.h>
 
 /**
-	*main - Entry point
-	*return: 1
+	*main - this the main function
+	*return: 1 - when sucessfull
+
 	
 */
 int main(void)
 {
       write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    
-   return (1);
+    return (1);
 
 }
