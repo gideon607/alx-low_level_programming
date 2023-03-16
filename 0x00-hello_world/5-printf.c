da74c554f82741f0d5b95b1d6318f6901a44e391
gideon607 <gideonojigi@gmail.com>
2023-03-16 16:41:27 +0100

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the mani function
+ *
+ * Return 0 if successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art.\n");
+	return (0);
+}
