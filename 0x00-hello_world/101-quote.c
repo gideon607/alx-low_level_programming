b243caf1eeaacecb676ce053413efbce0bde0392
gideon607 <gideonojigi@gmail.com>
2023-03-16 18:29:55 +0100

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - this is the main function 
- * Return:1-if successful
+ * Return:1 if successful
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar,2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
