1bd2ea8b393f2307b310a92bbb4d5ed662e8f899
gideon607 <gideonojigi@gmail.com>
2023-03-17 22:56:42 +0100

    printing

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 - if sucessful
+ */
+int main(void)
+{int i;
+	for (i = 0 ; i < 10 ; i++)
+		prinf("%d", i);
+	printf("\n");
+	return (0);
+}
