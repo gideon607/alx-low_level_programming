7a27147e6c592a220fe57e55b085877e24b26fe8
gideon607 <gideonojigi@gmail.com>
2023-03-17 22:28:33 +0100

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is the main
+ *
+ * Return: 0 - if sucessful
+ */
+int main(void)
+{char i;
+for (i = 'a' ; i <= 'z' ; i++)
+	putchar(i);
+for (i = 'A' ; i <= 'z' ; i++)
+	putchar(i);
+putchar('\n');
+return (0);
+}
+
