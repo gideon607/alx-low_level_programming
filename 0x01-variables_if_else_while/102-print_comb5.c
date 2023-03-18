2a35a1cbc6ca05dd4eef2bef68e4dc9cccb9e6c0
gideon607 <gideonojigi@gmail.com>
2023-03-18 01:23:11 +0100

    prining combs

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,30 +3,32 @@
 /**
  * main - Entry point
  *
- *
  * Return: Always 0 (Success)
  */
-
-
 int main(void)
-{int first, second;
-for (first = 0; first < 100; first++)
 {
-for (second = 0; second < 100; second++)
-{
-if (first < second)
-{putchar((first / 10) + 48);
-putchar((first % 10) + 48);
-putchar(' ');
-putchar((second / 10) + 48);
-putchar((second % 10) + 48);
-if (first != 98 || second != 99)
-{putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	int first, second;
+
+	for (first = 0; first < 100; first++)
+	{
+		for (second = 0; second < 100; second++)
+		{
+			if (first < second)
+			{
+				putchar((first / 10) + 48);
+				putchar((first % 10) + 48);
+				putchar(' ');
+				putchar((second / 10) + 48);
+				putchar((second % 10) + 48);
+
+				if (first != 98 || second != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
