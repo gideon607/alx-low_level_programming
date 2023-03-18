d64c252ad836ab6a5815105fac588b7d99d3deca
gideon607 <gideonojigi@gmail.com>
2023-03-18 01:02:13 +0100

    10

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{int first, second;
+for (first = 0; first < 100; first++)
+{
+for (second = 0; second < 100; second++)
+{
+if (first < second)
+{putchar((first / 10) + 48);
+putchar((first % 10) + 48);
+putchar(' ');
+putchar((second / 10) + 48);
+putchar((second % 10) + 48);
+if (first != 98 || second != 99)
+{putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
