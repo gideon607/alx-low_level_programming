56b126208d0ba3671490de618dc867796b30df69
gideon607 <gideonojigi@gmail.com>
2023-05-04 22:08:54 +0100

    task 4

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_bit - sets an index to 1 at a given location.
+ * @n: pointer constant.
+ * @index: index of the bit set to 1.
+ *
+ * Return: 1 for successful, -1 for fail
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int masks;
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+	masks = 1UL << index;
+	*n |= mask;
+
+	return (1);
+}
